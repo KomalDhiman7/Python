7f75e0e3144cdfeb7ce8cc0ded308cf032714b18
Komal <156698112+KomalDhiman7@users.noreply.github.com>
2024-11-13 17:57:05 +0000

    python

--- DSA/Tree/del.py
+++ DSA/Tree/del.py
@@ -64,13 +64,49 @@ class BST:
             self._postorder(root.right, result)
             result.append(root.item)
 
-bst = BST()
+    def  min_value(self,temp):
+        current = temp
+        if current.left is not None:
+            current=current.left
+        return current.item
 
+    def max_value(self,temp):
+        current= temp
+        if current.right is not None:
+            current = current.right
+        return current.item
+
+    def delete(self,data):
+        self.root=self.rdelele(self.root,data)
+    
+    def rdelete(self,root,data):
+        if root is None:
+            return 0
+        elif data>root.item:
+            root.left= rdelete(self,root.left,data)
+        elif data<root.item:
+            root.right=rdelete(self,root.right,data)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            root.item= root.min_value(root.right)
+            self.rdelete(root.right,root.item)
+        return root
+    
+    
+
+
+
+bst = BST()
 values = [10, 50, 40, 70, 30, 90, 20]
 for value in values:
     bst.insert(value)
 
-
 print("Inorder traversal of the BST:", bst.inorder())
-print("Search for no. 40",bst.search(40))
-print("Postorder traversal of the BST  is:", bst.postorder())
+
+
+
+
+
